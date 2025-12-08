33cf5961be69b6e690171d726050b77638ed6549
Adam Victor Nazareth Brandizzi <adam@brandizzi.com.br>
2025-12-07 23:31:33 -0300

    Usando nome correto de banco; montando pasta com csvs dentro de container

--- startpostgres.sh
+++ startpostgres.sh
@@ -1,11 +1,12 @@
 docker run \
-    --name fbd202502 \
+    --name fbd20252 \
     --rm \
     -e POSTGRES_USER=postgres \
     -e POSTGRES_PASSWORD=Postgres123 \
-    -e POSTGRES_DB=fbd202502 \
+    -e POSTGRES_DB=fbd20252 \
     -p 5432:5432 \
     -v $PWD/postgres-data:/var/lib/postgresql/ \
+    -v $PWD/csv_dados:/var/lib/csv_dados/ \
     -it \
     -d postgres
 
